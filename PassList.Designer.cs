1f46bb520d854326d67d54eaf7db2cac319b83df
dommie123 <49500416+dommie123@users.noreply.github.com>
2020-10-11 00:29:09 -0600

    Updated Program
    I have made the following changes to the program:  
     - The program now refreshes automatically each time a user makes a query.
     - Removed the ability to manually refresh the list of passwords.

---
+++ PassList.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Password_Bank
+{
+    partial class PassList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pList = new System.Windows.Forms.ListView();
+            this.btnToggleHide = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pList
+            // 
+            this.pList.HideSelection = false;
+            this.pList.Location = new System.Drawing.Point(32, 29);
+            this.pList.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.pList.Name = "pList";
+            this.pList.Size = new System.Drawing.Size(1108, 874);
+            this.pList.TabIndex = 0;
+            this.pList.UseCompatibleStateImageBehavior = false;
+            this.pList.View = System.Windows.Forms.View.List;
+            // 
+            // btnToggleHide
+            // 
+            this.btnToggleHide.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToggleHide.Location = new System.Drawing.Point(32, 1069);
+            this.btnToggleHide.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnToggleHide.Name = "btnToggleHide";
+            this.btnToggleHide.Size = new System.Drawing.Size(1112, 72);
+            this.btnToggleHide.TabIndex = 1;
+            this.btnToggleHide.Text = "Hide/Show Passwords";
+            this.btnToggleHide.UseVisualStyleBackColor = true;
+            this.btnToggleHide.Click += new System.EventHandler(this.btnToggleHide_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(29, 945);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(339, 72);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "New Password";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(421, 945);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(339, 72);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update Password";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(805, 945);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(339, 72);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete Password";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // PassList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 1174);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnToggleHide);
+            this.Controls.Add(this.pList);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "PassList";
+            this.Text = "Password Bank";
+            this.Load += new System.EventHandler(this.PassList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView pList;
+        private System.Windows.Forms.Button btnToggleHide;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ PassList.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Bank
+{
+    public partial class PassList : Form
+    {
+        private bool passIsHidden = true;   // Is the password "hidden"?
+
+        public PassList()
+        {
+            InitializeComponent();
+        }
+
+        // "New Password" button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewPass newPassForm = new NewPass();
+            newPassForm.ShowDialog();
+
+            if (!newPassForm.Visible) RefreshList();
+        }
+
+        // "Update Password" button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // If passwords are not hidden, open the "Update" form with the selected parameters.
+                if (!passIsHidden)
+                {
+                    ListViewItem selectedItem = pList.SelectedItems[0];
+
+                    UpdatePass updateForm = new UpdatePass();
+
+                    updateForm.Name = pList.SelectedItems[0].SubItems[0].Text;      // User Name
+                    updateForm.Company = pList.SelectedItems[0].SubItems[1].Text;   // Company Name
+                    updateForm.Password = pList.SelectedItems[0].SubItems[2].Text;  // Password
+
+                    updateForm.ShowDialog();
+
+                    if (!updateForm.Visible) RefreshList();
+                }
+                else // If passwords are hidden, prompt the user to show the passwords before continuing.
+                {
+                    MessageBox.Show("Please show passwords before updating selected item.", "Passwords are hidden");
+                }
+            } catch (ArgumentOutOfRangeException)
+            {
+                // If no password is selecetd, prompt the user to select a password.
+                MessageBox.Show("Please select a password to update!", "No Password Selected");
+            }
+        }
+
+        // "Delete Password" button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string confirmMessage = "Are you sure you want to delete this password?";
+                string confirmTitle = "Password Deletion";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show(confirmMessage, confirmTitle, buttons);
+
+                if (result == DialogResult.Yes)
+                {
+                    ListViewItem itemToRemove = pList.SelectedItems[0];
+                    string itemAsString = itemToRemove.Text;
+                    bool itemFound = false;
+                    try
+                    {
+                        StreamReader reader = new StreamReader("passwords.txt");
+                        LinkedList<string> lines = new LinkedList<string>();
+
+                        while (!reader.EndOfStream)
+                        {
+                            lines.AddLast(reader.ReadLine());
+                        }
+
+                        reader.Close();
+                        File.Delete("passwords.txt");
+
+                        StreamWriter writer = new StreamWriter("passwords.txt", true);
+                        string[] linesAsArray = lines.ToArray();
+
+                        for (int i = 0; i < linesAsArray.Length; i++)
+                        {
+                            if ((linesAsArray[i].Substring(0, linesAsArray[i].IndexOf(",")) != itemAsString) || itemFound)
+                            {
+                                writer.WriteLine(linesAsArray[i]);
+                            }
+                            else
+                            {
+                                itemFound = true;
+                            }
+                        }
+
+                        RefreshList();
+                        writer.Close();
+
+                    }
+                    catch (IOException exception)
+                    {
+                        MessageBox.Show(exception.Message, "I/O Exception Occurred");
+                    }
+                    finally
+                    {
+                        pList.Items.Remove(itemToRemove);
+                    }
+                }
+            } catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("Please select a password to delete!", "No Password Selected");
+            }
+        }
+
+        private void btnToggleHide_Click(object sender, EventArgs e)
+        {
+            passIsHidden = !passIsHidden;
+            RefreshList();
+        }
+
+        /** This method is obsolete because the program refreshes itself after user interaction.
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+        **/
+
+        private void PassList_Load(object sender, EventArgs e)
+        {
+            pList.View = View.Details;
+            pList.FullRowSelect = true;
+
+            pList.Columns.Add("Name", 100);
+            pList.Columns.Add("Company", 150);
+            pList.Columns.Add("Password", 165);
+
+            UpdateList();
+        }
+
+        public void UpdateList()
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader("passwords.txt");
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+
+                    string name = line.Substring(0, line.IndexOf(","));
+                    string company = line.Substring(line.IndexOf(",") + 2, line.LastIndexOf(",") - (name.Length + 2));
+                    string password = line.Substring(line.LastIndexOf(",") + 2);
+
+                    Password newPass = new Password(password, name, company);
+
+                    if (passIsHidden) newPass.ToggleHidden();
+
+                    string[] arr = new string[3];
+                    ListViewItem item;
+
+                    arr[0] = newPass.User;
+                    arr[1] = newPass.Company;
+                    arr[2] = newPass.PassDisplay;
+
+                    item = new ListViewItem(arr);
+                    pList.Items.Add(item);
+                }
+                streamReader.Close();
+
+            }
+            catch (IOException)
+            {
+                // If the passwords.txt file does not exist, create it.
+                StreamWriter streamWriter = new StreamWriter("passwords.txt");
+                streamWriter.Close();
+                //Console.WriteLine(exception.Message);
+            }
+        }
+
+        private void RefreshList()
+        {
+            pList.Items.Clear();
+            UpdateList();
+        }
+    }
+}
